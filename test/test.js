70fb1a6191a032a540343f7d3992ae4f0524d713
Bala Nair <bnairtm@gmail.com>
2017-08-04 07:59:46 -0400

    playing around with DRYing the test fixtures

--- test/test.js
+++ test/test.js
@@ -6,6 +6,11 @@ var math   = require('mathjs');
 describe('Connect4', () => {
   var board;
 
+  let rows2test = [
+    [ [[0,0],[0,1],[0,2]], [0,3], true],
+    [ [[0,0],[0,1],[0,2]], [0,4], false]
+  ];
+
   let fill = (points, val) => {
     points.forEach((point) => {
       board.set(point, val);
@@ -48,22 +53,24 @@ describe('Connect4', () => {
     });
   });
 
+  // test rows
   describe('test horizontals', () => {
     it('should return a winner when [0,0] to [0,3] are filled', (done) => {
-      fill([[0,0],[0,1],[0,2]], 1);
-      let play = c4.play_bruteforce(board, [0,3]);
-      expect(play.winner).to.be.true;
+      fill(rows2test[0][0], 1);
+      let play = c4.play_bruteforce(board, rows2test[0][1]);
+      expect(play.winner).to.be.equal(rows2test[0][2]);
       done();
     });
 
     it('should return no winner when [0,0] to [0,2] and [0,4] are filled', (done) => {
-      fill([[0,0],[0,1],[0,2]], 1);
-      let play = c4.play_bruteforce(board, [0,4]);
-      expect(play.winner).to.be.false;
+      fill(rows2test[1][0], 1);
+      let play = c4.play_bruteforce(board, rows2test[1][1]);
+      expect(play.winner).to.be.equal(rows2test[1][2]);
       done();
     });
   });
 
+  // test columns
   describe('test verticals', () => {
     it('should return a winner when [0,0] to [3,0] are filled', (done) => {
       fill([[0,0],[1,0],[2,0]], 1);
